ec77691fa25410149774e2c032fecdc98766686e
Pavel <shved64@mail.ru>
2024-09-10 11:55:19 +0400

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/tracker/oop/DummyDic.java
@@ -0,0 +1,13 @@
+package ru.job4j.tracker.oop;
+
+public class DummyDic {
+    public String engToRus(String eng) {
+        return "Неизвестное слово. " + eng;
+    }
+
+    public static void main(String[] args) {
+        DummyDic dic = new DummyDic();
+        String result = dic.engToRus("Hello");
+        System.out.println(result);
+    }
+}
