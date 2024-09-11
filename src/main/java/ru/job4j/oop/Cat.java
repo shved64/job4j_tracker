f0213c9ba699fa37af020f90bf2655420598cf6a
Pavel <shved64@mail.ru>
2024-09-11 11:02:23 +0400

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,22 +1,22 @@
-package ru.job4j.tracker.oop;
+package ru.job4j.oop;
 
 public class Cat {
 
     private String name;
-
-    public void giveNick(String nick) {
-    this.name = nick;
-    }
-
     private String food;
 
-    public void show() {
-        System.out.println(this.name + ' ' + "eat " + this.food);
+    public void giveNick(String nick) {
+        this.name = nick;
     }
 
     public void eat(String meat) {
         this.food = meat;
     }
+
+    public void show() {
+        System.out.println(this.name + ' ' + "eat " + this.food);
+    }
+
 
     public static void main(String[] args) {
         System.out.println("There is gav's food.");
