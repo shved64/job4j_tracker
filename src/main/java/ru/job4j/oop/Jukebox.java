e3ef0c0af3729e4323898f759982e2a92e9a64e6
Pavel <shved64@mail.ru>
2024-09-11 10:58:39 +0400

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -0,0 +1,24 @@
+package ru.job4j.oop;
+
+public class Jukebox {
+    public void music(int position) {
+        switch (position) {
+            case 1:
+                System.out.println("Пусть бегут неуклюже");
+                break;
+            case 2:
+                System.out.println("Спокойной ночи");
+                break;
+            default:
+                System.out.println("Песня не найдена");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        Jukebox disk = new Jukebox();
+        disk.music(1);
+        disk.music(2);
+        disk.music(3);
+    }
+}
