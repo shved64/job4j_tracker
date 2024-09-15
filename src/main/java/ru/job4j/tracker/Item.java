9b9115e9d4dd6b00cd9f2dd71be995a9b22c599b
Pavel <shved64@mail.ru>
2024-09-15 10:41:40 +0400

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -7,9 +7,6 @@ public class Item {
     private String name;
     private LocalDateTime created = LocalDateTime.now();
 
-    public LocalDateTime getCreated() {
-        return created;
-    }
 
     public Item() {
     }
@@ -37,6 +34,10 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
 }
