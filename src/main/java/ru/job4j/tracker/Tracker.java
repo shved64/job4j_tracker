2378f21fce878fcd234e8e2630fa6ee023358b71
Pavel <shved64@mail.ru>
2024-09-16 12:47:15 +0400

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -61,5 +61,22 @@ public class Tracker {
         return false;
     }
 
+    public void delete(int id) {
+        int index = indexOf(id);
+
+        if (index != -1) {
+            items[index] = null;
+
+            int start = index + 1;
+            int distPos = index;
+            int length = size - index - 1;
+            System.arraycopy(items, start, items, distPos, length);
+            items[size - 1] = null;
+            size--;
+        }
+    }
 }
+
+
+
 
