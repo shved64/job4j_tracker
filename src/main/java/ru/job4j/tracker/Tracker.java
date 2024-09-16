0b72c0df9ef1c1fef7d6506f92c99eb47287b13f
Pavel <shved64@mail.ru>
2024-09-16 12:59:54 +0400

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -53,18 +53,19 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        if (index != -1) {
+        boolean result = index != -1;
+        if (result) {
             item.setId(id);
             items[index] = item;
             return true;
         }
-        return false;
+        return result;
     }
 
     public void delete(int id) {
         int index = indexOf(id);
-
-        if (index != -1) {
+        boolean result = index != -1;
+        if (result) {
             items[index] = null;
 
             int start = index + 1;
