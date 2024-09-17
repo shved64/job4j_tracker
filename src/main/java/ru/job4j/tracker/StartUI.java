b7e6896826d2505bc52d6faef27e9847a0bd7e79
Pavel <shved64@mail.ru>
2024-09-17 20:54:43 +0400

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,7 +6,9 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
+        Item item1 = new Item();
         LocalDateTime createdDate = item.getCreated();
+        System.out.println(item1);
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String formattedDate = createdDate.format(formatter);
