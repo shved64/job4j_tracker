b7c8b6f11d19d62e5d2607088b5c423657a764ff
Pavel <shved64@mail.ru>
2024-09-19 11:03:29 +0400

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/cast/Transport.java
@@ -0,0 +1,14 @@
+package ru.job4j.cast;
+
+public class Transport {
+
+    public static void main(String[] args) {
+        Vehicle airplane = new Airplane();
+        Vehicle train = new Train();
+        Vehicle bus = new Bus();
+        Vehicle[] vehicles = new Vehicle[]{airplane, train, bus};
+        for (Vehicle object : vehicles) {
+            object.move();
+        }
+    }
+}
