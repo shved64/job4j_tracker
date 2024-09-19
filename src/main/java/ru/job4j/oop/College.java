16be1d2ec9cddebe65020a9cf543c70db4ada033
Pavel <shved64@mail.ru>
2024-09-19 10:39:34 +0400

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/oop/College.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+
+public class College {
+
+    public static void main(String[] args) {
+        Freshman freshman = new Freshman();
+
+        Student1 student1 = freshman;
+
+        Object object = freshman;
+    }
+}
