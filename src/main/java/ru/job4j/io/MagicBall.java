33e285f5c7d9e770ebd2d1e53269f1f0901789db
Pavel <shved64@mail.ru>
2024-09-19 09:10:19 +0400

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -11,15 +11,9 @@ public class MagicBall {
         String name = input.nextLine();
         int answer = new Random().nextInt(3);
         switch (answer) {
-            case 0:
-                System.out.println("Да");
-                break;
-            case 1:
-                System.out.println("Нет");
-                break;
-            default:
-                System.out.println("Может быть");
-                break;
+            case 0 -> System.out.println("Да");
+            case 1 -> System.out.println("Нет");
+            default -> System.out.println("Может быть");
         }
     }
 }
