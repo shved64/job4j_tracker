0655cba488aa4e79bff6af8f40a987f85ddaab14
Pavel <shved64@mail.ru>
2024-09-20 11:31:16 +0400

    Репозиторий под проект Tracker

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -15,4 +15,26 @@ class StartUITest {
         Item expected = new Item("Fix PC");
         assertThat(created.getName()).isEqualTo(expected.getName());
     }
+
+    @Test
+    void replaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item();
+        tracker.add(item);
+        String[] answer = {String.valueOf(item.getId()), "edited item"};
+        StartUI.replaceItem(new MockInput(answer), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName()).isEqualTo("edited item");
+    }
+
+    @Test
+    void deleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item();
+        tracker.add(item);
+        String[] answer = {String.valueOf(item.getId()), "edited item"};
+        Item edited = tracker.findById(item.getId());
+        tracker.delete(item.getId());
+        assertThat(edited.getName()).isEqualTo(null);
+    }
 }
