e31c02b6e5d28e5453a06a60b4be2bcea50f8a46
Pavel <shved64@mail.ru>
2024-09-20 21:30:21 +0400

    Репозиторий под проект Tracker

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -5,36 +5,5 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class StartUITest {
 
-    @Test
-    void createItem() {
-        String[] answers = {"Fix PC"};
-        Input input = new MockInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertThat(created.getName()).isEqualTo(expected.getName());
-    }
 
-    @Test
-    void replaceItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item();
-        tracker.add(item);
-        String[] answer = {String.valueOf(item.getId()), "edited item"};
-        StartUI.replaceItem(new MockInput(answer), tracker);
-        Item edited = tracker.findById(item.getId());
-        assertThat(edited.getName()).isEqualTo("edited item");
-    }
-
-    @Test
-    void deleteItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item();
-        tracker.add(item);
-        String[] answer = {String.valueOf(item.getId()), "edited item"};
-        Item edited = tracker.findById(item.getId());
-        tracker.delete(item.getId());
-        assertThat(edited.getName()).isEqualTo(null);
-    }
 }
