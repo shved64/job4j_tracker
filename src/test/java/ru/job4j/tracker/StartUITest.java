faae3f003877ee8920bdc85cd0304cf69f6e419d
Pavel <shved64@mail.ru>
2024-09-21 09:26:36 +0400

    Репозиторий под проект Tracker

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -5,5 +5,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class StartUITest {
 
+    @Test
+    void deleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item();
+        tracker.add(item);
+        String[] answer = {String.valueOf(item.getId()), "edited item"};
+        tracker.delete(item.getId());
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited).isNull();
+    }
 
 }
