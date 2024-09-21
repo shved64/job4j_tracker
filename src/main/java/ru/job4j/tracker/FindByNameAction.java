64bcf7b64c750fe27694c80067a37db76497e063
Pavel <shved64@mail.ru>
2024-09-21 09:28:59 +0400

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/tracker/FindByNameAction.java
+++ src/main/java/ru/job4j/tracker/FindByNameAction.java
@@ -18,6 +18,6 @@ public class FindByNameAction implements UserAction {
         } else {
             System.out.println("Заявки с именем: " + name + " не найдены.");
         }
-        return false;
+        return true;
     }
 }
