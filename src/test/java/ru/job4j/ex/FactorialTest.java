267beb8ef5e135c4aeef3a07a52435e7a56520cf
Pavel <shved64@mail.ru>
2024-09-22 16:37:13 +0400

    Репозиторий под проект Tracker

---
+++ src/test/java/ru/job4j/ex/FactorialTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.ex;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class FactorialTest {
+
+    @Test
+     public void whenException() {
+        Factorial factorial = new Factorial();
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    factorial.calc(-1);
+                }
+        );
+        assertThat(exception.getMessage()).isEqualTo("Number could not be less than 0");
+    }
+}
