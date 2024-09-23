9b1355faa909e510a3383ae81ce6968f3ddd57f4
Pavel <shved64@mail.ru>
2024-09-23 20:34:49 +0400

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,33 @@
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+
+public class SingleTracker {
+
+    private Tracker tracker = new Tracker();
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public void delete(int id) {
+        tracker.delete(id);
+    }
+}
+
