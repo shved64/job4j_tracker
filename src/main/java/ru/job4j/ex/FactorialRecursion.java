7c72e47a10aac06516653207fdcb4a0914d94ac0
Pavel <shved64@mail.ru>
2024-09-24 11:17:52 +0400

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/ex/FactorialRecursion.java
+++ src/main/java/ru/job4j/ex/FactorialRecursion.java
@@ -4,10 +4,10 @@ public class FactorialRecursion {
     public static int calc(int n) {
         if (n == 0 || n == 1) {
             return 1;
-        } else {
+        }
             return calc(n - 1) * n;
         }
-    }
+
 
     public static void main(String[] args) {
         int result = calc(3);
