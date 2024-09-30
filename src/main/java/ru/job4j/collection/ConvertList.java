582149bf86093c41ca25b3eec7362f411fa95911
Pavel <shved64@mail.ru>
2024-09-30 19:36:01 +0400

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/collection/ConvertList.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConvertList {
+    public static List<Integer> convert(List<int[]> list) {
+        List<Integer> result = new ArrayList<>();
+        for (int[] array : list) {
+            for (int cell : array) {
+                result.add(cell);
+
+            }
+
+        }
+        return result;
+    }
+}
