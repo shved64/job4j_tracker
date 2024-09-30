e94eb0a7b2ef17491b73833d92adca1d012f6332
Pavel <shved64@mail.ru>
2024-09-30 15:30:06 +0400

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -13,9 +13,9 @@ public class PhoneDictionary {
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
             if (person.getName().contains(key) ||
-            person.getSurname().contains(key) ||
-            person.getPhone().contains(key) ||
-            person.getAddress().contains(key)) {
+                    person.getSurname().contains(key) ||
+                    person.getPhone().contains(key) ||
+                    person.getAddress().contains(key)) {
                 result.add(person);
             }
         }
