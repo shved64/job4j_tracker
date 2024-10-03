84d2ac8c39700562364871f8626718ae94b47013
Pavel <shved64@mail.ru>
2024-10-03 11:10:13 +0400

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,8 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("shved64@mail.ru", "Kravchuk Pavel Andreevich");
+        map.put("shved6@mail.ru", "Tkachuk Pavel Andreevich");
+        map.put("shved64@mail.ru", "Kravchuk Pavel Andreevich");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
