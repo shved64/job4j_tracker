de5def09badea396d9f10e510b959eea46a8801d
Pavel <shved64@mail.ru>
2024-10-09 12:25:18 +0400

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -5,14 +5,10 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int minLength = Math.min(left.length(), right.length());
-
-        for (int i = 0; i < minLength; i++) {
-            char leftChar = left.charAt(i);
-            char rightChar = right.charAt(i);
-
-            if (leftChar != rightChar) {
-                return Character.compare(leftChar, rightChar);
+        for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
+            int result = Character.compare(left.charAt(i), right.charAt(i));
+            if (result != 0) {
+                return result;
             }
         }
         return Integer.compare(left.length(), right.length());
