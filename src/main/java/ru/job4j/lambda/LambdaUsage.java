4d3848dfd5b90ea217e09cdfd49746bd0171add1
Pavel <shved64@mail.ru>
2024-10-14 11:24:39 +0400

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -9,7 +9,7 @@ public class LambdaUsage {
     public static void main(String[] args) {
         List<String> strings = Arrays.asList("eeeee", "a",  "ccc", "dddd", "bb");
         Comparator<String> comparator = (left, right) -> {
-            System.out.println("string- " + right.length() + ":" + left.length());
+            System.out.println("string- " + left.length() + ":" + right.length());
            return Integer.compare(right.length(), left.length());
         };
         strings.sort(comparator);
